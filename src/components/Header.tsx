9354782176e925580dc42764d817751ff0d7324f
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-27 15:45:40 -0300

    chore: removed the plus button from the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import * as I from "react-icons/bs";
 
 const Header = () => {
   return (
-    <header className="w-full h-[6vh] bg-pet-contrast flex items-center justify-between px-8">
+    <header className="w-full h-[6vh] bg-pet-contrast flex items-center justify-center px-8">
       <I.BsFillPersonFill className="text-white text-3xl" />
-      <I.BsPlus className="text-white text-3xl" />
     </header>
   );
 };
