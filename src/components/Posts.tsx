c858d0fd0d6dc5e11ff728d895b3de83a33ab99b
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-31 11:49:14 -0300

    reafactor: separated the main content from page to a component

---
+++ src/components/Posts.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { mockData } from "@/mock/post";
+import Image from "next/image";
+import * as I from "react-icons/bs";
+
+const Posts = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      {mockData.map((post) => (
+        <div key={post.id} className="mb-4">
+          <div className="flex items-center gap-2 bg-[#121212] py-2 pl-1">
+            <I.BsFillPersonFill className="text-white h-[25px] w-[25px]" />
+            <p className="text-white text-sm">matheusosan</p>
+          </div>
+          <Image
+            width={480}
+            height={920}
+            src={post.imageUrl}
+            alt={post.description}
+            className="h-[60vh] w-full bg-fit bg-center"
+          />
+          <div className="flex flex-col pl-1 py-3 gap-2">
+            <button>
+              <I.BsFillHeartFill className="text-white" />
+            </button>
+            <p className="font-bold text-sm text-white">matheusosan</p>
+            <h2 className="text-sm text-white">{post.description}</h2>
+            <p className="text-xs text-gray-400">Há 2 dias</p>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default Posts;
