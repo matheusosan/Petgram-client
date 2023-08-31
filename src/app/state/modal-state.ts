7cdf80ebfe537601407da047c4469674ef56df9e
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-31 11:48:21 -0300

    feat: created the modal state with zustand

---
+++ src/app/state/modal-state.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type ModalProps = {
+  isModalOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+};
+
+export const useModalStore = create<ModalProps>((set) => ({
+  isModalOpen: false,
+  openModal: () => set({ isModalOpen: true }),
+  closeModal: () => set({ isModalOpen: false }),
+}));
