ab314c4305c1a12da6ffa1d0616eb43030b59562
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-31 20:57:32 -0300

    style: fixed post size

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,22 +5,20 @@ import * as I from "react-icons/bs";
 
 const Posts = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex h-full flex-col items-center justify-center">
       {mockData.map((post) => (
-        <div key={post.id} className="mb-4">
+        <div key={post.id} className="pb-6">
           <div className="flex items-center gap-2 bg-[#121212] py-2 pl-1">
             <I.BsFillPersonFill className="text-white h-[25px] w-[25px]" />
             <p className="text-white text-sm">matheusosan</p>
           </div>
-          <div className="h-auto w-full">
-            <Image
-              width={480}
-              height={920}
-              src={post.imageUrl}
-              alt={post.description}
-              className="w-full aspect-auto bg-center"
-            />
-          </div>
+          <Image
+            width={480}
+            height={920}
+            src={post.imageUrl}
+            alt={post.description}
+            className=" w-full aspect-square bg-cover bg-center"
+          />
           <div className="flex flex-col pl-1 py-3 gap-2">
             <button>
               <I.BsFillHeartFill className="text-white" />
