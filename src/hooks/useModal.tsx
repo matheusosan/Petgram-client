995269c1c7f61dc2f4144b648874a14140a5095c
Matheus Santana <matheussantanaoli@gmail.com>
2023-09-02 12:27:15 -0300

    feat: created modal custom hook to lock body scroll if the modal is open

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,18 @@
+import { useModalStore } from "@/app/state/modal-state";
+import { useEffect } from "react";
+
+export const useModal = () => {
+  const { isModalOpen } = useModalStore();
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isModalOpen]);
+
+  return {
+    isModalOpen,
+  };
+};
