25dfb87f2b186e8c77d5ed6201a905521a8c2f70
Matheus Santana <matheussantanaoli@gmail.com>
2023-10-13 03:13:39 -0300

    url

--- src/hooks/useHandleLogin.ts
+++ src/hooks/useHandleLogin.ts
@@ -3,12 +3,15 @@ import { LoginType } from "@/components/Login";
 export const useHandleLogin = () => {
   const onSubmit = async (data: LoginType) => {
     try {
-      const response = await fetch("http://localhost:3000/user/login", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-      });
+      const response = await fetch(
+        "https://petgram-server.onrender.com/auth/login",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        }
+      );
 
       const dados = await response.json();
 
