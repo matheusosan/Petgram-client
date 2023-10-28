3a1b21bf546f77f9d73f30cea2fffc99fd6743c9
Matheus Santana <matheussantanaoli@gmail.com>
2023-10-28 12:01:01 -0300

    feat: added server fetch to get data from authenticated user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,26 @@
+import { cookies } from "next/headers";
 import React from "react";
 import * as I from "react-icons/bs";
 
-const Header = () => {
+const Header = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("access_token")?.value;
+
+  const res = await fetch("http://localhost:3000/user", {
+    credentials: "include",
+    headers: {
+      Authorization: "Bearer " + token,
+      "x-auth-token": `${token}`,
+
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  console.log(data);
+
   return (
     <header className="w-full h-[6vh] bg-pet-contrast flex items-center justify-center px-8">
+      <h1>Olá {data.username}</h1>
       <I.BsFillPersonFill className="text-white text-3xl" />
     </header>
   );
