9998ae441561b16e5ef44c9be8db938bea435cdd
Matheus Santana <matheussantanaoli@gmail.com>
2023-10-28 12:03:25 -0300

    feat: toast container tothe  app

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import LoginForm from "@/components/Login";
+import Toast from "@/components/toast";
 import React from "react";
 
 const page = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <LoginForm />
+      <Toast />
     </div>
   );
 };
