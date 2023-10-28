655c13a653dda75ec2ec5698e86e197a5d77b84d
Matheus Santana <matheussantanaoli@gmail.com>
2023-10-28 11:58:57 -0300

    refactor: added toast to login sucessfull and error

--- src/hooks/useHandleLogin.ts
+++ src/hooks/useHandleLogin.ts
@@ -1,26 +1,50 @@
 import { LoginType } from "@/components/Login";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export const useHandleLogin = () => {
+  const router = useRouter();
+
   const onSubmit = async (data: LoginType) => {
     try {
-      const response = await fetch(
-        "https://petgram-server.onrender.com/auth/login",
-        {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: { "Content-Type": "application/json" },
-          credentials: "include",
-        }
-      );
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
 
-      const dados = await response.json();
-
-      console.log(dados);
+      if (response.ok) {
+        toast("Login bem sucedido!", {
+          theme: "dark",
+        });
+        router.push("/");
+      }
     } catch (error) {
-      console.log(`Ocorreu um erro: ${error}`);
+      toast(`Ocorreu um erro: ${error}`);
     }
   };
+
+  const onLogout = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+      }
+
+      console.log(response);
+    } catch (error) {
+      toast(`Ocorreu um erro: ${error}`);
+    }
+  };
+
   return {
     onSubmit,
+    onLogout,
   };
 };
